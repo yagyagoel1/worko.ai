145b06dc96ebcf2797c0bfbee710272db00031cb
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-23 16:30:07 +0530

    added the templates for api response error and asynchandler

---
+++ src/utils/ApiError.js
@@ -0,0 +1,10 @@
+class ApiError {
+    constructor(status,  message = "Something went Wrong",
+        errors = [],data=null) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+        this.data = data;
+    }
+}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+
+
+const asyncHandler = (fn) =>async (req, res, next) =>
+{
+    try{
+      await  fn(req,res,next);
+    }
+    catch(error){
+        res.status(500).json({message:"internal server error"});
+        console.log("error while processing request",error);
+    }
+}
