d14f987d36786e19dfd3db7e0a387eef3909baf3
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-23 19:45:26 +0530

    added the main route header

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const limiter = rateLimit({
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  message: "Too many requests from this IP, please try again after 15 minutes",});
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
 const app = express();
 
 //middleware declaration
@@ -36,5 +37,7 @@ app.get(
   })
 );
 
+import workoRouter from "./routes/index.route.js";
+app.use("/worko", workoRouter);
 
 export default app;
