5fcdb320ee6b700a27cd930ec098173d300d8079
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-23 15:06:01 +0530

    added the middlewares

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message: "Too many requests from this IP, please try again after 15 minutes",});
+const app = express();
+
+//middleware declaration
+app.use(limiter);
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, //to allow cookies from the client
+  })
+);
+app.disable("x-powered-by");
+app.use(express.json({ limit: "50kb" }));
+app.use(express.urlencoded({ extended: true, limit: "50kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//health check route
+app.get(
+  "/",
+  asyncHandler((req, res) => {
+    res.status(200).json({ message: "Is Healthy" });
+  })
+);
+
+
+export default app;
